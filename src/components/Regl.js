de4fe9033a26556d9d2d04a8d25935d1dc64439d
sathis <sathiskumar.m@myntra.com>
2018-04-22 21:37:01 +0530

    Fix ref

--- src/components/Regl.js
+++ src/components/Regl.js
@@ -1,102 +1,113 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import ReglRenderer from '../renderer.js';
-import reglInit from 'regl';
+import React from "react";
+import PropTypes from "prop-types";
+import ReglRenderer from "../renderer.js";
+import reglInit from "regl";
 
-import ReglRootNode from '../nodes/ReglRootNode';
+import ReglRootNode from "../nodes/ReglRootNode";
 
 export default class Regl extends React.Component {
-  regl = null
-  tick = null;
+    regl = null;
+    tick = null;
 
-  componentWillUnmount(){
-    this.regl.destroy();
-  }
-
-  constructor(props, context){
-    super(props, context);
-    this.rootNode = null;
-  }
-
-  getChildContext(){
-    if(this.context.store){
-      return {store: this.context.store};
-    }
-  }
-
-  componentDidMount(){
-    const canvasRef = this.props.canvas || this.refs.canvas;
-    const gl = canvasRef.getContext("webgl", {
-      alpha: false,
-      antialias: false,
-      stencil: false,
-      preserveDrawingBuffer: false
-    });
-
-    const regl = reglInit({ gl });
-    regl.cache = {};
-    this.regl = regl;
-
-
-    if(this.props.onFrame){
-      this.tick = regl.frame(this.props.onFrame);
+    componentWillUnmount() {
+        this.regl.destroy();
     }
 
-    if(this.props.onReglInit &&
-       typeof this.props.onReglInit === 'function'){
-      this.props.onReglInit(regl);
+    constructor(props, context) {
+        super(props, context);
+        this.rootNode = null;
     }
 
-    this.rootNode = ReglRenderer.createContainer(new ReglRootNode(regl, this.context));
-
-    this.regl.clear({
-      color: this.props.color || [0, 0, 0, 1],
-      depth: this.props.depth || 1
-    });
-
-    ReglRenderer.unbatchedUpdates(() => {
-      ReglRenderer.updateContainer(this.props.children, this.rootNode, this);
-    });
-
-    this.rootNode.containerInfo.render()
-  }
-
-  componentDidUpdate(prevProps, prevState){
-    this.regl.clear({
-      color: this.props.color || [0, 0, 0, 1],
-      depth: this.props.depth || 1
-    });
-
-    if(prevProps.width !== this.props.width ||
-       prevProps.height !== this.props.height){
-      this.regl.poll();
+    getChildContext() {
+        if (this.context.store) {
+            return { store: this.context.store };
+        }
     }
 
-    ReglRenderer.updateContainer(this.props.children, this.rootNode, this);
-    this.rootNode.containerInfo.render();
-  }
+    componentDidMount() {
+        const canvasRef = this.props.canvas || this.canvas;
+        const gl = canvasRef.getContext("webgl", {
+            alpha: false,
+            antialias: false,
+            stencil: false,
+            preserveDrawingBuffer: false
+        });
 
-  componentWillUnmount(){
-    if(this.tick){
-      this.tick.cancel();
+        const regl = reglInit({ gl });
+        regl.cache = {};
+        this.regl = regl;
+
+        if (this.props.onFrame) {
+            this.tick = regl.frame(this.props.onFrame);
+        }
+
+        if (
+            this.props.onReglInit &&
+            typeof this.props.onReglInit === "function"
+        ) {
+            this.props.onReglInit(regl);
+        }
+
+        this.rootNode = ReglRenderer.createContainer(
+            new ReglRootNode(regl, this.context)
+        );
+
+        this.regl.clear({
+            color: this.props.color || [0, 0, 0, 1],
+            depth: this.props.depth || 1
+        });
+
+        ReglRenderer.unbatchedUpdates(() => {
+            ReglRenderer.updateContainer(
+                this.props.children,
+                this.rootNode,
+                this
+            );
+        });
+
+        this.rootNode.containerInfo.render();
     }
 
-    if(this.regl){
-      this.regl.destroy();
-    }
-  }
+    componentDidUpdate(prevProps, prevState) {
+        this.regl.clear({
+            color: this.props.color || [0, 0, 0, 1],
+            depth: this.props.depth || 1
+        });
 
-  render(){
-    if(this.props.canvas){
-      return null;
+        if (
+            prevProps.width !== this.props.width ||
+            prevProps.height !== this.props.height
+        ) {
+            this.regl.poll();
+        }
+
+        ReglRenderer.updateContainer(this.props.children, this.rootNode, this);
+        this.rootNode.containerInfo.render();
     }
 
-    return (
-      <canvas ref="canvas"
-              width={this.props.width}
-              height={this.props.height} />
-    )
-  }
+    componentWillUnmount() {
+        if (this.tick) {
+            this.tick.cancel();
+        }
+
+        if (this.regl) {
+            this.regl.destroy();
+        }
+    }
+
+    render() {
+        if (this.props.canvas) {
+            return null;
+        }
+
+        return (
+            <canvas
+                ref={ref => (this.canvas = ref)}
+                width={this.props.width}
+                height={this.props.height}
+            />
+        );
+    }
 }
 // In order to support react-redux apps
 // the store needs to be passed to the reconciler on context
